772336ba70597a6986428d89cbfc40d85dbfa435
Nishanth Kiran <nishanthkiran2@gmail.com>
2023-05-01 10:19:45 +0530

    wezterm configuration remap update

--- wezterm/remap.lua
+++ wezterm/remap.lua
@@ -5,8 +5,15 @@ local act = wezterm.action
 
 function module.apply_to_config(config)
     config.keys = {
+        -- for toggling between tabs
         { key = '[', mods = 'CTRL', action = act.ActivateTabRelative(-1) },
         { key = ']', mods = 'CTRL', action = act.ActivateTabRelative(1) },
+
+        -- for opening a new tab
+        { key = 'n', mods = 'CTRL', action = act.SpawnCommandInNewTab},
+
+        -- for closing current tab
+        { key = 'w', mods = 'CTRL', action = act.CloseCurrentTab{confirm=true}},
     }
 end
 
